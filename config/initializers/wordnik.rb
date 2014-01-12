e430af5429b0e6d20cd77d7b3a09a1ff044f7aa7
cohart <me@colin-hart.com>
2014-01-12 14:24:06 -0500

    removed env in omniauth

--- config/initializers/wordnik.rb
+++ config/initializers/wordnik.rb
@@ -1,5 +1,5 @@
 Wordnik.configure do |config|
-  config.api_key = '0216617c9a267a477c0040dd0140d78aa5cccfaba9f6d78f5'               # required
+  config.api_key = ENV['WORDNIK_KEY']               # required
   config.username = 'coha'                    # optional, but needed for user-related functions
   config.password = 'rousseau'               # optional, but needed for user-related functions
   config.response_format = 'json'             # defaults to json, but xml is also supported
