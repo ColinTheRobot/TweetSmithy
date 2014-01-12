86ba305d49da555bfc3d4d14e42d4af21b3ab4ba
cohart <me@colin-hart.com>
2014-01-12 14:03:02 -0500

    Update twitter initializer

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -5,7 +5,3 @@ client = Twitter::REST::Client.new do |config|
   config.oauth_token_secret = ENV["OAUTH_SECRET_SMITHY"]
 end
 
-AWS::Twitter::Base.establish_connection!(
-  :access_key_id     => ENV['TWITTER_KEY_SMITHY']
-  :secret_access_key => ENV['TWITTER_SECRET_SMITHY']
-  end
