4ac681f0fd7e0eb724d290d4a01465d88c2e9000
cohart <me@colin-hart.com>
2014-01-12 13:37:47 -0500

    oauth fix for heroku

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -4,3 +4,8 @@ client = Twitter::REST::Client.new do |config|
   config.oauth_token = ENV["OAUTH_TOKEN_SMITHY"]
   config.oauth_token_secret = ENV["OAUTH_SECRET_SMITHY"]
 end
+
+AWS::Twitter::Base.establish_connection!(
+  :access_key_id     => ENV['TWITTER_KEY_SMITHY']
+  :secret_access_key => ENV['TWITTER_SECRET_SMITHY']
+  end
