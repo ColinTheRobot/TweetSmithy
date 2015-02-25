f77610b09b21b9713aaec5c6c84d052302f05784
cohart <me@colin-hart.com>
2015-02-25 00:05:15 -0500

    cleaned up tweeting controller. there is one method that needs to be looked at before removing

--- app/controllers/tweeting_controller.rb
+++ app/controllers/tweeting_controller.rb
@@ -1,25 +1,19 @@
 class TweetingController < ApplicationController
   require 'twitter'
 
-
-#method 1
+# NOT SURE WHAT THIS POST METHOD IS YET
   # def post
   #   @post = Tweet.new(params[:post])
   #   current_user.update "Tweet"
   #   @tweeting = Tweet.new
   # end
 
-  #method 2
   def new
     @tweet = Tweet.new
   end
 
   def create
     current_user.tweet(params[:message])
-    
-  # current_user.upda(twitter_params[:message])
-  #  @tweet = Tweet.new(twitter_params)[:message]
-    
   end
 
   def twitter_params
