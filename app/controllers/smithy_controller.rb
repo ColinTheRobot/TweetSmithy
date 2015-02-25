0d34bbb9d6a07649c6c5062d4be9ad5fa74e45af
cohart <me@colin-hart.com>
2015-02-25 00:09:20 -0500

    Remove unneeded method

--- app/controllers/smithy_controller.rb
+++ app/controllers/smithy_controller.rb
@@ -4,12 +4,6 @@ class SmithyController < ApplicationController
      #@synonyms = Wordnik.word.get_related
   end
 
-  # def create
-  #   @post = Post.new(params[:post])
-  #   Twitter.update("I'm tweeting with @gem!")
-  #   #code
-  # end
-
   def initialize_wordnik(options={})
     options.merge!(ENV['WORDNIK_API_KEY'])
     @wordnik = Wordnik::Wordnik.new(options)
