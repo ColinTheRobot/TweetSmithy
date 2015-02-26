deeaf22e9560dbd2309d8e105d2e3a32303e781b
cohart <me@colin-hart.com>
2015-02-25 22:33:19 -0500

    did stuff to initializer

--- config/initializers/wordnik.rb
+++ config/initializers/wordnik.rb
@@ -1,7 +1,7 @@
 Wordnik.configure do |config|
-  config.api_key = ENV['WORDNIK_API_KEY']               # required
-  config.username = 'coha'                    # optional, but needed for user-related functions
-  config.password = 'rousseau'               # optional, but needed for user-related functions
-  config.response_format = 'json'             # defaults to json, but xml is also supported
-  config.logger = Logger.new('/dev/null')     # defaults to Rails.logger or Logger.new(STDOUT). Set to Logger.new('/dev/null') to disable logging.
+  config.api_key = ENV['WORDNIK_API_KEY']
+  config.username = 'coha'
+  config.password = 'rousseau'
+  config.response_format = 'json'
+  config.logger = Logger.new('/dev/null')
 end
