e7d59a343961408b6923b8ca90ac099d140fc08c
cohart <me@colin-hart.com>
2015-02-25 21:52:20 -0500

    Refactor wordnik controller

--- app/controllers/wordnik_controller.rb
+++ app/controllers/wordnik_controller.rb
@@ -1,16 +1,21 @@
 class WordnikController < ApplicationController
   def index
-    # initialize_wordnik will set up the @wordnik object, which we can then use to call the wordnik api.
   end
 
   def search
-    query = params[:search_word].split
-    @words = {}
-    query.each do |word|
-      wordnik_results = Wordnik.word.get_related(word, :type => 'synonym')
-      unless wordnik_results.empty?
-        @words[word] = wordnik_results.first['words']
-      end
+    words = {}
+
+    parsed_query.each do |word|
+      synonyms = Wordnik.word.get_related(word, type: 'synonym')
+
+      words[word] = synonyms.first['words'] unless synonyms.empty?
     end
+  end
+
+
+  private
+
+  def parsed_query
+    params[:search_word].split
   end
 end
