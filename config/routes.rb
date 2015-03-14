8aacadef2b71f96f1e75676cb6363362429303d8
cohart <me@colin-hart.com>
2015-03-14 10:48:33 -0400

    Modify routes to reflect changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   root 'smithy#index'
   get '/about', to: 'pages#about'
-  get '/search', to: 'wordnik#search'
-  # post '/search', to: 'tweeting#new'
+  get '/search', to: 'smithy#search'
+
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  resources :sessions, only: [:create, :destroy]
+
   post '/tweet', to: 'tweeting#create', as: 'tweet'
     # resources :tweets, only: [:new, :create]
-    resources :sessions, only: [:create, :destroy]
     resource :home, only: [:show]
 
 end
