7b71e1087339a1b90283ba431c5b581d25b6ecd7
cohart <me@colin-hart.com>
2015-03-29 11:45:58 -0400

    Add logic to parse the length of words/smybols/synoyms and handle empty results

--- app/controllers/smithy_controller.rb
+++ app/controllers/smithy_controller.rb
@@ -1,16 +1,11 @@
 class SmithyController < ApplicationController
 
   def index
+    binding.pry
   end
 
   def search
-    @words = {}
-
-    parsed_query.each do |word|
-      synonyms = Wordnik.word.get_related(word, type: 'synonym')
-
-      @words[word] = synonyms.first['words'] unless synonyms.empty?
-    end
+    get_synonyms
 
     respond_to do |format|
       format.html {render :index}
@@ -20,7 +15,51 @@ class SmithyController < ApplicationController
 
   private
 
-  def parsed_query
-    params[:search_word].split
+  def get_synonyms
+    split_params
+  end
+
+  def split_params
+   @parsed_query = params[:search_word].split
+   remove_symbols
+  end
+
+  def remove_symbols
+    @parsed_query.each do |word|
+      word.gsub!(/\W|\d/, '')
+    end
+    clean_words
+  end
+
+  def clean_words
+    @parsed_query.delete_if do |word|
+      word.length < 3 || word.blank?
+    end
+    wordnik_query
+  end
+
+  def wordnik_query
+    @words = {}
+    @parsed_query.each do |word|
+      synonyms = Wordnik.word.get_related(word, type: 'synonym')
+
+      @words[word] = synonyms.first['words'] unless synonyms.empty?
+    end
+    parse_synonyms
+  end
+
+  def parse_synonyms
+    @words.each_key do |word|
+      length_of_word = word.length
+      
+      @words["#{word}"].delete_if do |syn|
+        syn.length >= length_of_word
+      end
+    end
+    remove_empties
+  end
+
+  def remove_empties
+    @words.delete_if { |key, value| value.blank? }
   end
 end
